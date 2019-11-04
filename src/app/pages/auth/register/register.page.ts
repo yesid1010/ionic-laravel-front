aab423164fec16fd8fefe8a75471f1929e3ad7dc
yesid1010 <sanchezyesid72@gmail.com>
2019-11-04 01:09:00 -0500

    first commit

---
+++ src/app/pages/auth/register/register.page.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController, NavController } from '@ionic/angular';
+import { LoginPage } from '../login/login.page';
+import { AuthService } from 'src/app/services/auth.service';
+import { NgForm } from '@angular/forms';
+import { AlertService } from 'src/app/services/alert.service';
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+  constructor(private modalController: ModalController,
+    private authService: AuthService,
+    private navCtrl: NavController,
+    private alertService: AlertService
+  ) { }
+  ngOnInit() {
+  }
+  // Dismiss Register Modal
+  dismissRegister() {
+    this.modalController.dismiss();
+  }
+  // On Login button tap, dismiss Register modal and open login Modal
+  async loginModal() {
+    this.dismissRegister();
+    const loginModal = await this.modalController.create({
+      component: LoginPage,
+    });
+    return await loginModal.present();
+  }
+  register(form: NgForm) {
+    this.authService.register(form.value.first_name, form.value.last_name, form.value.email, form.value.password).subscribe(
+      data => {
+        this.authService.login(form.value.email, form.value.password).subscribe(
+          data => {
+            console.log(data)
+          },
+          error => {
+            console.log(error);
+          },
+          () => {
+            this.dismissRegister();
+            this.navCtrl.navigateRoot('/dashboard');
+          }
+        );
+        this.alertService.presentToast(data['message']);
+      },
+      error => {
+        console.log(error);
+      },
+      () => {
+        
+      }
+    );
+  }
+}
